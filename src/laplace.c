4aa42f6ee6cd7a21758862cba7de99e4b1c1f37d
david.laroche <david.laroche@etu.hesge.ch>
2023-04-15 09:16:25 +0200

    time added

--- src/laplace.c
+++ src/laplace.c
@@ -11,6 +11,14 @@
 
 #define ROOT 0
 
+void init_grid_test(double* U, int max_I, int max_J) {
+    
+    for (int i=0; i<max_I; i++) {
+        for (int j=0; j<max_J; j++)
+            U[i*max_J+j] = i;
+    }
+}
+
 void init_temp(double* U, int max_I, int max_J) {
         
     for (int i=0; i<max_I; i++)
@@ -26,52 +34,28 @@ void init_temp(double* U, int max_I, int max_J) {
     }
 }
 
-void init_grid_test(double* U, int max_I, int max_J) {
-    
-    for (int i=0; i<max_I; i++) {
-        for (int j=0; j<max_J; j++)
-            U[i*max_J+j] = i;
-    }
+void update_row(double *old_row, double *new_row, int max_J) {
+    for (int i=0; i<max_J; i++)
+        old_row[i] = new_row[i];
 }
 
-void grid_evol(double* U, int max_I, int max_J, int max_T) {
-    double* U_temp = (double*) malloc(max_I * max_J * sizeof(double));
-
-    for (int t = 0; t < max_T; t++) {
-        for (int i = 1; i < max_I - 1; i++) {
-            for (int j = 1; j < max_J - 1; j++) {
-                int index = i * max_J + j;
-                U_temp[index] = 0.25 * (U[(i - 1) * max_J + j] + U[(i + 1) * max_J + j] +
-                    U[i * max_J + j - 1] + U[i * max_J + j + 1]);
-            }
-        }
-        for (int i = 1; i < max_I - 1; i++) {
-            for (int j = 1; j < max_J - 1; j++) {
-                int index = i * max_J + j;
-                U[index] = U_temp[index];
-            }
-        }
-    }
-    free(U_temp);
-}
-
-void band_evol(double* band, int max_I, int max_J) {
-    double* band_temp = (double*) malloc(max_I * max_J * sizeof(double));
+void mat_evol(double* mat, int max_I, int max_J) {
+    double* mat_temp = (double*) malloc(max_I * max_J * sizeof(double));
 
     for (int i = 1; i < max_I - 1; i++) {
         for (int j = 1; j < max_J - 1; j++) {
             int index = i * max_J + j;
-            band_temp[index] = 0.25 * (band[(i - 1) * max_J + j] + band[(i + 1) * max_J + j] +
-                band[i * max_J + j - 1] + band[i * max_J + j + 1]);
+            mat_temp[index] = 0.25 * (mat[(i - 1) * max_J + j] + mat[(i + 1) * max_J + j] +
+                mat[i * max_J + j - 1] + mat[i * max_J + j + 1]);
         }
     }
     for (int i = 1; i < max_I - 1; i++) {
         for (int j = 1; j < max_J - 1; j++) {
             int index = i * max_J + j;
-            band[index] = band_temp[index];
+            mat[index] = mat_temp[index];
         }
     }
-    free(band_temp);
+    free(mat_temp);
 }
 
 void get_band_sz(int* band_sz, int nProc, int size) {
@@ -132,16 +116,23 @@ void print_mat(double* mat, int rows, int cols)
     printf("\n");
 }
 
-void update_row(double *old_row, double *new_row, int max_J) {
-    for (int i=0; i<max_J; i++)
-        old_row[i] = new_row[i];
+void print_mat_ref(int max_I, int max_J, int max_T) {
+    double *U_test = calloc(max_I*max_J, sizeof(double));
+    init_temp(U_test, max_I, max_J);
+
+    for (int t = 0; t < max_T; t++)
+        mat_evol(U_test, max_I, max_J);
+
+    printf("mat ref:\n");
+    print_mat(U_test, max_I, max_J);
+    free(U_test);
 }
 
 int main(int argc, char** argv) {
 	(void)argc;
-
+    
     int myRank, nProc;
-  
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &nProc);    
@@ -150,16 +141,13 @@ int main(int argc, char** argv) {
     int max_J = atoi(argv[2]); // colonnes
     int max_T = atoi(argv[3]); // temps discret
 
-    double *U, *U_res = NULL;
+    double *U = NULL;
 
     if(myRank == ROOT) { // Root create grid
         U = calloc(max_I*max_J, sizeof(double));
         init_temp(U, max_I, max_J);
-        U_res = calloc(max_I*max_J, sizeof(double));
-        double *U_test = calloc(max_I*max_J, sizeof(double));
-        init_temp(U_test, max_I, max_J);
-        grid_evol(U_test, max_I, max_J, max_T);
-        print_mat(U_test, max_I, max_J);
+        //print_mat(U, max_I, max_J);
+        //print_mat_ref(max_I, max_J, max_T);
     }
     
     int myRows[nProc];
@@ -169,8 +157,7 @@ int main(int argc, char** argv) {
     get_band_sz(myRows, nProc, max_I); // get number of rows per process
     compute_scatterv_params(myRows, sendcounts, displs, nProc, max_J);
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    double* myBand = malloc(sendcounts[myRank]* sizeof(double));
+    double* myBand = malloc(sendcounts[myRank] * sizeof(double));
     
     MPI_Scatterv(U, sendcounts, displs, MPI_DOUBLE, myBand, sendcounts[myRank], MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 
@@ -179,10 +166,13 @@ int main(int argc, char** argv) {
     double *recv_top_row = malloc(max_J* sizeof(double)); 
     double *recv_bottom_row = malloc(max_J* sizeof(double));
 
+    clock_t start_time = clock();
     for(int t=0; t<max_T; t++) {
+
         MPI_Barrier(MPI_COMM_WORLD);
-        band_evol(myBand, myRows[myRank], max_J);
+        mat_evol(myBand, myRows[myRank], max_J);
         MPI_Barrier(MPI_COMM_WORLD);
+
         send_top_row = myBand + max_J;
         send_bottom_row = myBand + (myRows[myRank] -2) * max_J;
 
@@ -197,8 +187,7 @@ int main(int argc, char** argv) {
                 MPI_Recv(recv_top_row, max_J, MPI_DOUBLE, myRank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 update_row(myBand, recv_top_row, max_J);
             }
-        }
-        else
+        } else
         {
             if (myRank != nProc-1) {
                 MPI_Recv(recv_bottom_row, max_J, MPI_DOUBLE, myRank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -212,18 +201,18 @@ int main(int argc, char** argv) {
             }
         }
     }
-
-    MPI_Gatherv(myBand, sendcounts[myRank], MPI_DOUBLE, U_res, sendcounts, displs, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    clock_t end_time = clock();
+    MPI_Gatherv(myBand, sendcounts[myRank], MPI_DOUBLE, U, sendcounts, displs, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
     
     if(myRank == ROOT) {
-        printf("RES\n");
-        print_mat(U_res, max_I, max_J);
-        output_mat(U_res, max_I, max_J);
+        //printf("Result\n");
+        //print_mat(U, max_I, max_J);
+        //output_mat(U, max_I, max_J);
+        printf("Elapsed time: %.3f seconds\n", ((double) (end_time - start_time)) / CLOCKS_PER_SEC);
         free(U);
     }
     free(recv_top_row);
     free(recv_bottom_row);
     free(myBand);
-    free(U_res);
     MPI_Finalize();
 }
